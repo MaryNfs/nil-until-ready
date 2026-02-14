4216f6d2469db73b70b19d0cca0270692a5361bc
MaryNfs <mm10arya@gmail.com>
2026-02-14 16:49:48 +0330

    Added Valid Palindrome

---
+++ Go/Leetcode-125. Valid Palindrome/valid_palindrome.go
@@ -0,0 +1,29 @@
+package valid_palindrome
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func isPalindrome(s string) bool {
+
+	reg := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	s = reg.ReplaceAllString(s, "")
+	s = strings.ToLower(s)
+
+	arr := strings.Split(s, "")
+	if len(arr) < 1 {
+		return true
+	}
+	i := 0
+	j := len(arr) - 1
+	for i < j {
+		if arr[i] != arr[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
