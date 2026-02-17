af211b168d1e6b79e008db3ea2844b04321afb86
MaryNfs <mm10arya@gmail.com>
2026-02-17 13:57:42 +0800

    added test

--- Go/Leetcode/two_sum/two_sum_test.go
+++ Go/Leetcode/two_sum/two_sum_test.go
@@ -1,7 +1,22 @@
 package two_sum
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestTosum(t *testing.T) {
-	t.Fatal("write tests")
+	cases := []struct {
+		numbers []int
+		targert int
+		output  []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+	}
+	for _, c := range cases {
+		res := twoSum(c.numbers, c.targert)
+		if !slices.Equal(res, c.output) {
+			t.Errorf("twoSum(%v, %v) = %v, expected %v", c.numbers, c.targert, res, c.output)
+		}
+	}
 }
