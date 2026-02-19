90b35a5f2ab8b6bdeb6150b1f59e89307da8e995
MaryNfs <mm10arya@gmail.com>
2026-02-19 23:57:12 +0800

    xor solution for single_number

--- Go/Leetcode/single_number/single_number.go
+++ Go/Leetcode/single_number/single_number.go
@@ -1,14 +1,24 @@
 package single_number
 
+// Map solution
+// func singleNumber(nums []int) int {
+//     h := make(map[int]int,0)
+//     for _,n := range nums {
+//         h[n]++
+//     }
+//     for key,_ := range h{
+//         if h[key] ==1 {
+//             return key
+//         }
+//     }
+//     return 0
+// }
+
+// XOR Solution
 func singleNumber(nums []int) int {
-    h := make(map[int]int,0)
+    res :=0
     for _,n := range nums {
-        h[n]++
+        res = n^res
     }
-    for key,_ := range h{
-        if h[key] ==1 {
-            return key
-        }
-    }
-    return 0
+    return res
 }
