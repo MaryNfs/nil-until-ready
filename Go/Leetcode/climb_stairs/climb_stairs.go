d035095adb7263b839900bc55360f7b81f074534
MaryNfs <mm10arya@gmail.com>
2026-02-20 09:04:39 +0800

    third try

--- Go/Leetcode/climb_stairs/climb_stairs.go
+++ Go/Leetcode/climb_stairs/climb_stairs.go
@@ -12,12 +12,29 @@ package climb_stairs
 
 // Magic way
 // https://www.youtube.com/shorts/x4epBH6HF10
-func climbStairs(n int) int {
-	one := 1
-	two := 1
+// func climbStairs(n int) int {
+// 	one := 1
+// 	two := 1
 
-	for i := 0; i < n-1; i++ {
-		one, two = two, two+one
+// 	for i := 0; i < n-1; i++ {
+// 		one, two = two, two+one
+// 	}
+// 	return two
+// }
+
+// Third try
+func climbStairs(n int) int {
+	if n == 1 || n == 2 {
+		return n
 	}
-	return two
+
+	one, two := 1, 2
+	var res int
+	
+	for i := 2; i <= n-1; i++ {
+		res = one + two
+		one = two
+		two = res
+	}
+	return res
 }
