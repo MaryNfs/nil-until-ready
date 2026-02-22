f13408d0596803dc26019eb6f25197a2fe0f8c54
MaryNfs <mm10arya@gmail.com>
2026-02-22 20:34:20 +0800

    count_bits

--- Go/Leetcode/count_bits/count_bits.go
+++ Go/Leetcode/count_bits/count_bits.go
@@ -1,6 +1,29 @@
-package count_bits
+package countBits
 
-// TODO: implement solution
-func Solve() {
-	// implement
+// Ugly
+func countBits(n int) []int {
+	res := make([]int, 0)
+	for i := 0; i <= n; i++ {
+		binary := binary_Calc(i)
+		res = append(res, binary)
+	}
+	return res
+}
+
+func binary_Calc(n int) int {
+	res := make([]int, 0)
+	for n >= 2 {
+		mod := n % 2
+		res = append(res, mod)
+		n = n / 2
+
+	}
+	res = append(res, n)
+	count := 0
+	for i := 0; i < len(res); i++ {
+		if res[i] == 1 {
+			count++
+		}
+	}
+	return count
 }
