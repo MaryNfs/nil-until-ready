627dddc6ed700987c44ae2a42ae5ed5de9e725f2
MaryNfs <mm10arya@gmail.com>
2026-02-22 19:48:52 +0800

    testcases + description updated

--- Go/Leetcode/min_cost_climbing_stairs/min_cost_climbing_stairs_test.go
+++ Go/Leetcode/min_cost_climbing_stairs/min_cost_climbing_stairs_test.go
@@ -2,6 +2,19 @@ package min_cost_climbing_stairs
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestMinCostClimbingStairs(t *testing.T) {
+	cases := []struct {
+		cost []int
+		out  int
+	}{
+		{[]int{10, 15, 20}, 15},
+		{[]int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}, 6},
+	}
+
+	for _, c := range cases {
+		res := minCostClimbingStairs(c.cost)
+		if res != c.out {
+			t.Errorf("minCostClimbingStairs(%v)=%v, expected %v", c.cost, res, c.out)
+		}
+	}
 }
