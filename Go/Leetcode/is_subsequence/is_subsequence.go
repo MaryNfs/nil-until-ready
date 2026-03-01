0671390110527cec8efc672fcc11033e353f3fcb
MaryNfs <mm10arya@gmail.com>
2026-03-01 22:41:18 +0800

    added solution for is_subsequence

--- Go/Leetcode/is_subsequence/is_subsequence.go
+++ Go/Leetcode/is_subsequence/is_subsequence.go
@@ -1,6 +1,27 @@
 package is_subsequence
 
-// TODO: implement solution
+import (
+	"fmt"
+	"strings"
+)
+
 func isSubsequence(s string, t string) bool {
-    
+	sar := strings.Split(s, "")
+	tar := strings.Split(t, "")
+	j := 0
+	if len(sar) == 0 {
+		return true
+	}
+	for i := 0; i < len(tar); i++ {
+		if j < len(sar) {
+			if sar[j] == tar[i] {
+				j++
+			}
+		}
+	}
+	if j == len(sar) {
+		return true
+	}
+	return false
 }
+
