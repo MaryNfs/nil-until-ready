0a3e3bc735f23aef07890b93a305c5b782052768
MaryNfs <mm10arya@gmail.com>
2026-03-02 09:22:14 +0800

    added solution for good_node

--- Go/Leetcode/good_node/good_node.go
+++ Go/Leetcode/good_node/good_node.go
@@ -1,6 +1,28 @@
 package good_node
 
-// TODO: implement solution
-func Solve() {
-	// implement
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func goodNodes(root *TreeNode) int {
+	return dfs(root, root.Val)
+}
+
+func dfs(root *TreeNode, maxVal int) int {
+	var res int
+	if root == nil {
+		return 0
+	}
+	if root.Val >= maxVal {
+		res = 1
+	} else {
+		res = 0
+	}
+	maxVal = max(root.Val, maxVal)
+	res += dfs(root.Right, maxVal)
+	res += dfs(root.Left, maxVal)
+
+	return res
 }
