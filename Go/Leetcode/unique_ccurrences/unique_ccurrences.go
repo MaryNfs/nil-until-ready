b1425610a87ff7a63b3028bb4a0734a7d1662b82
MaryNfs <mm10arya@gmail.com>
2026-03-06 22:50:05 +0800

    added solution for uniqueOccurrences

--- Go/Leetcode/unique_ccurrences/unique_ccurrences.go
+++ Go/Leetcode/unique_ccurrences/unique_ccurrences.go
@@ -1,6 +1,20 @@
 package unique_ccurrences
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func uniqueOccurrences(arr []int) bool {
+	hashmap := make(map[int]int, 0)
+	res := make(map[int]int, 0)
+
+	for i := 0; i < len(arr); i++ {
+		hashmap[arr[i]]++
+	}
+	for _, v := range hashmap {
+		_, is := res[v]
+		if is {
+			return false
+		} else {
+			res[v]++
+		}
+	}
+
+	return true
 }
--- Go/Leetcode/unique_ccurrences/unique_ccurrences_test.go
+++ Go/Leetcode/unique_ccurrences/unique_ccurrences_test.go
@@ -2,6 +2,20 @@ package unique_ccurrences
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestUniqueOccurrences(t *testing.T) {
+	cases := []struct {
+		arr []int
+		res bool
+	}{
+		{[]int{1, 2, 2, 1, 1, 3}, true},
+		{[]int{1, 2}, false},
+		{[]int{-3, 0, 1, -3, 1, 1, 1, -3, 10, 0}, true},
+		{[]int{1}, true},
+	}
+	for _, c := range cases {
+		res := uniqueOccurrences(c.arr)
+		if res != c.res {
+			t.Errorf("uniqueOccurrences(%v)=%v but expected: %v", c.arr, res, c.res)
+		}
+	}
 }
