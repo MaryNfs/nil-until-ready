2c01f5ee24e077908d917f67f418f0d8d487b085
MaryNfs <mm10arya@gmail.com>
2026-03-06 22:02:00 +0800

    added testst for kidsWithCandies

--- Go/Leetcode/kids_with_candies/kids_with_candies_test.go
+++ Go/Leetcode/kids_with_candies/kids_with_candies_test.go
@@ -1,7 +1,25 @@
 package kids_with_candies
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestKidsWithCandies(t *testing.T) {
+	cases := []struct {
+		candies      []int
+		extraCandies int
+		res          []bool
+	}{
+		{[]int{2, 3, 5, 1, 3}, 3, []bool{true, true, true, false, true}},
+		{[]int{4, 2, 1, 1, 2}, 1, []bool{true, false, false, false, false}},
+		{[]int{12, 1, 12}, 10, []bool{true, false, true}},
+	}
+
+	for _, c := range cases {
+		res := kidsWithCandies(c.candies, c.extraCandies)
+		if !slices.Equal(res, c.res) {
+			t.Errorf("kidsWithCandies(%v,%v)=%v, but expected: %v", c.candies, c.extraCandies, res, c.res)
+		}
+	}
 }
