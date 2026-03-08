6a5afacd57ddc84c62485558d6da2ccaf31f53ea
MaryNfs <mm10arya@gmail.com>
2026-03-08 17:20:52 +0800

    largestAltitude solution added

--- Go/Leetcode/largest_altitude/largest_altitude.go
+++ Go/Leetcode/largest_altitude/largest_altitude.go
@@ -1,6 +1,18 @@
 package largest_altitude
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func largestAltitude(gain []int) int {
+    alt := make([]int, 0)
+    alt = append(alt,0)
+    pre := 0
+    for i:=0;i<len(gain);i++{
+        pre += gain[i]
+        alt = append(alt,pre)
+    }
+    max := alt[0]
+    for i:=0;i<len(alt);i++{
+        if alt[i]> max{
+            max = alt[i]
+        }
+    }
+    return max
 }
