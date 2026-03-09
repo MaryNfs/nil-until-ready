f6022b5668dd48792b90d1a5b51b2b59e90b04ef
MaryNfs <mm10arya@gmail.com>
2026-03-09 23:29:52 +0800

    added tests for largestAltitude

--- Go/Leetcode/largest_altitude/largest_altitude_test.go
+++ Go/Leetcode/largest_altitude/largest_altitude_test.go
@@ -2,6 +2,19 @@ package largest_altitude
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestLargestAltitude(t *testing.T) {
+	cases := []struct{
+		gain []int
+		out int
+	}{
+		{[]int{-5,1,5,0,-7},1},
+		{[]int{-4,-3,-2,-1,4,3,2},0},
+	}
+
+	for _,c := range cases{
+		res := largestAltitude(c.gain)
+		if res != c.out {
+			t.Errorf("largestAltitude(%v)=%v, but expected %v",c.gain,res,c.out)
+		}
+	}
 }
