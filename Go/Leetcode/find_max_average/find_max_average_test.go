b68f5a5fbddcc298e0da7c0e31bfe1d1cc9b9e6a
MaryNfs <mm10arya@gmail.com>
2026-03-10 23:38:27 +0800

    added tests for findMaxAverage

--- Go/Leetcode/find_max_average/find_max_average_test.go
+++ Go/Leetcode/find_max_average/find_max_average_test.go
@@ -2,6 +2,20 @@ package find_max_average
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestFindMaxAverage(t *testing.T) {
+	cases := []struct {
+		nums []int
+		k    int
+		out  float64
+	}{
+		{[]int{1, 12, -5, -6, 50, 3}, 4, 12.75000},
+		{[]int{5}, 1, 5.00000},
+		{[]int{-1}, 1, -1.00000},
+	}
+	for _, c := range cases {
+		res := findMaxAverage(c.nums, c.k)
+		if res != c.out {
+			t.Errorf("findMaxAverage(%v, %v)=%v, but expected %v", c.nums, c.k, res, c.out)
+		}
+	}
 }
