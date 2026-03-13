313f9ac2ec81e5f9781179ed2337f0278b30c626
MaryNfs <mm10arya@gmail.com>
2026-03-13 23:55:02 +0800

    pivotIndex solution

--- Go/Leetcode/pivot_index/pivot_index.go
+++ Go/Leetcode/pivot_index/pivot_index.go
@@ -1,6 +1,25 @@
 package pivot_index
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func pivotIndex(nums []int) int {
+     for i:=0;i<len(nums);i++{
+        res := sumthem(nums,i)
+        if res == true {
+            return i
+        }
+     }
+     return -1
+}
+func sumthem(nums []int,index int)bool{
+    lsum :=0
+    rsum :=0
+    for i:=0;i<index;i++{
+        lsum += nums[i]
+    }
+    for i:=index+1;i<len(nums);i++{
+        rsum += nums[i]
+    }
+    if rsum == lsum {
+        return true
+    }
+    return false
 }
--- Go/Leetcode/pivot_index/pivot_index_test.go
+++ Go/Leetcode/pivot_index/pivot_index_test.go
@@ -3,5 +3,19 @@ package pivot_index
 import "testing"
 
 func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+	cases := []struct {
+		nums []int
+		out  int
+	}{
+		{[]int{1, 7, 3, 6, 5, 6}, 3},
+		{[]int{1, 2, 3}, -1},
+		{[]int{2, 1, -1}, 0},
+	}
+
+	for _, c := range cases {
+		res := pivotIndex(c.nums)
+		if res != c.out {
+			t.Errorf("pivotIndex(%v)=%v but expected %v.", c.nums, res, c.out)
+		}
+	}
 }
