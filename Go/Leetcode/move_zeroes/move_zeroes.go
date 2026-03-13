b3f262550dcd52a9fd5fc4c355efde7a57ed6c8b
MaryNfs <mm10arya@gmail.com>
2026-03-13 23:22:03 +0800

    moveZeroes better solution

--- Go/Leetcode/move_zeroes/move_zeroes.go
+++ Go/Leetcode/move_zeroes/move_zeroes.go
@@ -1,15 +1,27 @@
 package move_zeroes
 
-func moveZeroes(nums []int) {
-	j := 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 0 && i < len(nums)-1 {
-			j = i + 1
-			for j < len(nums)-1 && nums[j] == 0 {
-				j++
-			}
-			nums[i] = nums[j]
-			nums[j] = 0
-		}
-	}
+// func moveZeroes(nums []int) {
+// 	j := 0
+// 	for i := 0; i < len(nums); i++ {
+// 		if nums[i] == 0 && i < len(nums)-1 {
+// 			j = i + 1
+// 			for j < len(nums)-1 && nums[j] == 0 {
+// 				j++
+// 			}
+// 			nums[i] = nums[j]
+// 			nums[j] = 0
+// 		}
+// 	}
+// }
+
+// Best
+func moveZeroes(nums []int)  {
+    j:=0
+    for i:=0;i<len(nums);i++{
+        if nums[i] != 0{
+            nums[j],nums[i]=nums[i], nums[j]
+            j++
+        }
+    }
+
 }
