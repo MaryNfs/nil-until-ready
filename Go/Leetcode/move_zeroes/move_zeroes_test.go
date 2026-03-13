b357d5578e63391897760ab8d4d876c8ab6a51e1
MaryNfs <mm10arya@gmail.com>
2026-03-13 22:59:13 +0800

    added tests for moveZeroes

--- Go/Leetcode/move_zeroes/move_zeroes_test.go
+++ Go/Leetcode/move_zeroes/move_zeroes_test.go
@@ -1,7 +1,25 @@
 package move_zeroes
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+var nums []int
+
+func TestMoveZeroes(t *testing.T) {
+	cases := []struct {
+		in []int
+		ex []int
+	}{
+		{[]int{0, 1, 0, 3, 12}, []int{1, 3, 12, 0, 0}},
+		{[]int{0}, []int{0}},
+	}
+
+	for _, c := range cases {
+		moveZeroes(c.in)
+		if !slices.Equal(c.in, c.ex) {
+			t.Errorf("moveZeroes=%v, but it should be %v", c.in, c.ex)
+		}
+	}
 }
