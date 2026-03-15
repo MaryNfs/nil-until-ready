ab705dad8158782da55fecebac831834906dad35
MaryNfs <mm10arya@gmail.com>
2026-03-15 23:01:47 +0800

    topKFrequent solution added

--- Go/Leetcode/top_K_frequent/q.md
+++ Go/Leetcode/top_K_frequent/q.md
@@ -1,12 +1,24 @@
-# top_K_frequent
+# 347. Top K Frequent Elements
 
 ## Problem
-
-Paste the problem description here.
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
 ## Examples
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+
+Example 3:
+Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+Output: [1,2]
 
 ## Notes
 - Constraints:
-- Edge cases:
-- Complexity:
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
--- Go/Leetcode/top_K_frequent/top_K_frequent.go
+++ Go/Leetcode/top_K_frequent/top_K_frequent.go
@@ -1,6 +1,32 @@
 package top_K_frequent
 
-// TODO: implement solution
-func Solve() {
-	// implement
+import "sort"
+
+type KeyValuePair struct {
+	Key   int
+	Value int
+}
+
+func topKFrequent(nums []int, k int) []int {
+	hres := make(map[int]int, 0)
+	for i := 0; i < len(nums); i++ {
+		hres[nums[i]]++
+	}
+	var sortedByValue []KeyValuePair
+	for k, v := range hres {
+		sortedByValue = append(sortedByValue, KeyValuePair{k, v})
+	}
+	sort.Slice(sortedByValue, func(i, j int) bool {
+		return sortedByValue[i].Value > sortedByValue[j].Value
+	})
+	c := 1
+	var res []int
+	for _, v := range sortedByValue {
+		if c > k {
+			break
+		}
+		res = append(res, v.Key)
+		c++
+	}
+	return res
 }
