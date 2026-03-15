21067fa722b23450b2455a5a3e92ee0e0ebc8fe1
MaryNfs <mm10arya@gmail.com>
2026-03-15 23:06:47 +0800

    added topKFrequent tests

--- Go/Leetcode/top_K_frequent/top_K_frequent_test.go
+++ Go/Leetcode/top_K_frequent/top_K_frequent_test.go
@@ -1,7 +1,25 @@
 package top_K_frequent
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestTopKFrequent(t *testing.T) {
+	cases := []struct {
+		nums []int
+		k    int
+		out  []int
+	}{
+		{[]int{3, 3, 3, 1}, 2, []int{3, 1}},
+		{[]int{1}, 1, []int{1}},
+		{[]int{1, 2, 1, 2, 1, 2, 3, 1, 3, 2}, 2, []int{1, 2}},
+	}
+
+	for _, c := range cases {
+		res := topKFrequent(c.nums, c.k)
+		if !slices.Equal(res, c.out) {
+			t.Errorf("topKFrequent(%v,%v)=%v, but it should be %v", c.nums, c.k, res, c.out)
+		}
+	}
 }
