8b84744f3bc09ddb9efeb599528991eac28af8ac
MaryNfs <mm10arya@gmail.com>
2026-03-15 14:36:03 +0800

    added recentCounter solution

--- Go/Leetcode/recent_counter/recent_counter.go
+++ Go/Leetcode/recent_counter/recent_counter.go
@@ -1,6 +1,26 @@
 package recent_counter
 
-// TODO: implement solution
-func Solve() {
-	// implement
+type RecentCounter struct {
+	queue []int
 }
+
+func Constructor() RecentCounter {
+	return RecentCounter{
+		queue: make([]int, 0),
+	}
+}
+
+func (this *RecentCounter) Ping(t int) int {
+	this.queue = append(this.queue, t)
+	for len(this.queue) > 0 && this.queue[0] < t-3000 {
+		this.queue = this.queue[1:]
+	}
+
+	return len(this.queue)
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Ping(t);
+ */
