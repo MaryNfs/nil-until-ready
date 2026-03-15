f7abe0ff465c352ba959a90fe14fe144061eac69
MaryNfs <mm10arya@gmail.com>
2026-03-15 18:06:26 +0800

    added test for recentCounter

--- Go/Leetcode/recent_counter/recent_counter_test.go
+++ Go/Leetcode/recent_counter/recent_counter_test.go
@@ -2,6 +2,32 @@ package recent_counter
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestRecentCounter(t *testing.T) {
+	cases := []struct {
+		op   string
+		arg  int
+		want int
+	}{
+		{"RecentCounter", 0, 0},
+		{"ping", 1, 1},
+		{"ping", 100, 2},
+		{"ping", 3001, 3},
+		{"ping", 3002, 3},
+	}
+
+	var rc RecentCounter
+
+	for i, c := range cases {
+		switch c.op {
+		case "RecentCounter":
+			rc = Constructor()
+
+		case "ping":
+			got := rc.Ping(c.arg)
+			if got != c.want {
+				t.Errorf("step %d: Ping(%d) = %d, want %d",
+					i, c.arg, got, c.want)
+			}
+		}
+	}
 }
