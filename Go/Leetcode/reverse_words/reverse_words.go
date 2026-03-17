faa9c0901f8ce9d0284fd4ab1db2ff89465269a3
MaryNfs <mm10arya@gmail.com>
2026-03-17 23:23:44 +0800

    reverseWords done

--- Go/Leetcode/reverse_words/reverse_words.go
+++ Go/Leetcode/reverse_words/reverse_words.go
@@ -1,6 +1,20 @@
 package reverse_words
 
-// TODO: implement solution
-func Solve() {
-	// implement
+import (
+	"strings"
+)
+
+func reverseWords(s string) string {
+	res := strings.Split(strings.TrimSpace(s), " ")
+	f := ""
+	for i := len(res) - 1; i >= 0; i-- {
+		if strings.TrimSpace(res[i]) != "" {
+			if i == 0 {
+				f += res[i]
+			} else {
+				f += res[i] + " "
+			}
+		}
+	}
+	return f
 }
--- Go/Leetcode/reverse_words/reverse_words_test.go
+++ Go/Leetcode/reverse_words/reverse_words_test.go
@@ -2,6 +2,20 @@ package reverse_words
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestReverseWords(t *testing.T) {
+	cases := []struct {
+		input string
+		out   string
+	}{
+		{"a good   example", "example good a"},
+		{"the sky is blue", "blue is sky the"},
+		{"  hello world  ", "world hello"},
+	}
+
+	for _, c := range cases {
+		res := reverseWords(c.input)
+		if res != c.out {
+			t.Errorf("reverseWords(%v)=%v, but expected %v", c.input, res, c.out)
+		}
+	}
 }
