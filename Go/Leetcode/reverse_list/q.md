43c8e69390a7af3429a8d0751047096a6f10e193
MaryNfs <mm10arya@gmail.com>
2026-03-18 23:52:48 +0800

    added solution for reverseList

--- Go/Leetcode/reverse_list/q.md
+++ Go/Leetcode/reverse_list/q.md
@@ -1,12 +1,21 @@
-# reverse_list
+# 206. Reverse Linked List
 
 ## Problem
 
-Paste the problem description here.
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
 
 ## Examples
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Input: head = [1,2]
+Output: [2,1]
+
+Input: head = []
+Output: []
 
 ## Notes
 - Constraints:
-- Edge cases:
-- Complexity:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
--- Go/Leetcode/reverse_list/reverse_list.go
+++ Go/Leetcode/reverse_list/reverse_list.go
@@ -1,6 +1,32 @@
 package reverse_list
 
-// TODO: implement solution
-func Solve() {
-	// implement
+/**
+ * Definition for singly-linked list.
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var newhead *ListNode
+	if head == nil {
+		return newhead
+	}
+	tail := &ListNode{Val: 0, Next: nil}
+	newhead = tail
+	res := make([]int, 0)
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+	for i := len(res) - 1; i >= 0; i-- {
+		tail.Val = res[i]
+		if i != 0 {
+			tail.Next = &ListNode{Val: 0, Next: nil}
+			tail = tail.Next
+		}
+	}
+
+	return newhead
 }
