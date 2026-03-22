71c28a5f2e1735d1263c00e95c39d0250ef40d86
MaryNfs <mm10arya@gmail.com>
2026-03-22 14:49:31 +0800

    added solution for productExceptSelf

--- Go/Leetcode/product_except_self/product_except_self.go
+++ Go/Leetcode/product_except_self/product_except_self.go
@@ -1,6 +1,30 @@
 package product_except_self
 
-// TODO: implement solution
-func Solve() {
-	// implement
+// First solution
+func productExceptSelf(nums []int) []int {
+    post := make([]int,len(nums))
+    pre := make([]int,len(nums))
+    prod :=1
+    for i:=0;i<len(nums);i++{
+        prod *= nums[i]
+        pre[i]=prod
+    }
+    prod = 1
+    for i:=len(nums)-1;i>=0;i--{
+        prod *= nums[i]
+        post[i]=prod
+    }
+    res := make([]int,0)
+    xx := 0
+    for i:=0;i<len(nums);i++{
+        if i==0{
+            xx = post[i+1]
+        }else if i== len(nums)-1{
+            xx = pre[i-1]
+        }else{
+            xx = pre[i-1]*post[i+1]
+        }
+        res = append(res,xx)
+    }
+    return res
 }
