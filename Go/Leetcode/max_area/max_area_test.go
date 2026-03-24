8ec4a28e9fe708b55a82191990ebbf57d2609d71
MaryNfs <mm10arya@gmail.com>
2026-03-24 22:55:43 +0800

    added tests for maxArea

--- Go/Leetcode/max_area/max_area_test.go
+++ Go/Leetcode/max_area/max_area_test.go
@@ -1,7 +1,24 @@
 package max_area
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestMaxArea(t *testing.T) {
+	cases := []struct {
+		heights []int
+		output  int
+	}{
+		{[]int{1, 8, 6, 2, 5, 4, 8, 3, 7}, 49},
+		{[]int{1, 1}, 1},
+		{[]int{0, 2}, 0},
+		{[]int{8, 7, 2, 1}, 7},
+	}
+
+	for _, c := range cases {
+		res := maxArea(c.heights)
+		if res != c.output {
+			t.Errorf("maxArea(%v)=%v, expected %v", c.heights, res, c.output)
+		}
+	}
 }
