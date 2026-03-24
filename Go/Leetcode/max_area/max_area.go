098cc26bd33a2cbb09b23e137547436538ab3369
MaryNfs <mm10arya@gmail.com>
2026-03-24 22:36:22 +0800

    added first solution for maxArea

--- Go/Leetcode/max_area/max_area.go
+++ Go/Leetcode/max_area/max_area.go
@@ -1,6 +1,16 @@
 package max_area
 
-// TODO: implement solution
-func Solve() {
-	// implement
+// Time Limit Exceeded Solution
+func maxArea(height []int) int {
+    max := 0
+    for i:=0;i<len(height);i++{
+        for j:=i;j<len(height);j++{
+            min := min(height[i],height[j])
+            space := (j-i)*min
+            if space > max {
+                max = space
+            }
+        }
+    }
+    return max
 }
