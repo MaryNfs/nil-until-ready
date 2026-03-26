22c5691230f9d552642a4623e3c41346bf12fe37
MaryNfs <mm10arya@gmail.com>
2026-03-27 00:27:53 +0800

    timelimit solution for maxVawels

--- Go/Leetcode/max_vowels/max_vowels.go
+++ Go/Leetcode/max_vowels/max_vowels.go
@@ -1,6 +1,27 @@
 package max_vowels
 
-// TODO: implement solution
-func Solve() {
-	// implement
+import "strings"
+
+// TimeLimit
+func maxVowels(s string, k int) int {
+	arr := strings.Split(s, "")
+	vows := make(map[string]int, 0)
+	vows = map[string]int{"a": 1, "e": 1, "i": 1, "o": 1, "u": 1}
+	count := 0
+	max := 0
+	j := 0
+	for j <= len(arr)-k {
+		for i := j; i < j+k; i++ {
+			_, ok := vows[arr[i]]
+			if ok {
+				count++
+			}
+		}
+		if count > max {
+			max = count
+		}
+		count = 0
+		j++
+	}
+	return max
 }
