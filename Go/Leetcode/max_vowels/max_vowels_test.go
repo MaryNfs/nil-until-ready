b375ab2686e1e84f2bb9cb538cdeed00b6f2369e
MaryNfs <mm10arya@gmail.com>
2026-03-28 00:02:03 +0800

    added tests for maxVowels

--- Go/Leetcode/max_vowels/max_vowels_test.go
+++ Go/Leetcode/max_vowels/max_vowels_test.go
@@ -2,6 +2,21 @@ package max_vowels
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestMaxVowels(t *testing.T) {
+	cases := []struct {
+		s   string
+		k   int
+		out int
+	}{
+		{"abciiidef", 3, 3},
+		{"aeiou", 2, 2},
+		{"leetcode", 3, 2},
+		{"weallloveyou", 7, 4},
+	}
+	for _, c := range cases {
+		res := maxVowels(c.s, c.k)
+		if res != c.out {
+			t.Errorf("maxVowels(%v,%v)=%v but expected %v", c.s, c.k, res, c.out)
+		}
+	}
 }
