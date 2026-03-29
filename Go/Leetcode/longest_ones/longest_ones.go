8a488ca19ca946414851fe6ba67a57a669bdc6a5
MaryNfs <mm10arya@gmail.com>
2026-03-30 00:29:37 +0800

    added solution for longestOnes

--- Go/Leetcode/longest_ones/longest_ones.go
+++ Go/Leetcode/longest_ones/longest_ones.go
@@ -1,6 +1,21 @@
 package longest_ones
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func longestOnes(nums []int, k int) int {
+	l, count, res, flip := 0, 0, 0, 0
+	for r := 0; r < len(nums); r++ {
+        if nums[r]==0 {
+            flip++
+        }
+        count ++
+
+		if flip > k {
+			if nums[l] == 0 {
+				flip--
+			}
+			count--
+			l++
+		}
+		res = max(res, count)
+	}
+	return res
 }
