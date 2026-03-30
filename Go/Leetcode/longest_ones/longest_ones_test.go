835717622d44a4d5b830c97bf4e8725a7b5d303f
MaryNfs <mm10arya@gmail.com>
2026-03-30 23:57:41 +0800

    added tests for longestOnes

--- Go/Leetcode/longest_ones/longest_ones_test.go
+++ Go/Leetcode/longest_ones/longest_ones_test.go
@@ -2,6 +2,20 @@ package longest_ones
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestLongestOnes(t *testing.T) {
+	cases := []struct {
+		nums []int
+		k    int
+		out  int
+	}{
+		{[]int{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2, 6},
+		{[]int{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3, 10},
+		{[]int{0, 0}, 2, 2},
+	}
+	for _, c := range cases {
+		res := longestOnes(c.nums, c.k)
+		if c.out != res {
+			t.Errorf("longestOnes(%v,%v)=%v. but expected %v", c.nums, c.k, res, c.out)
+		}
+	}
 }
