b040bc4a913d5e47aed046375091798dfdd8ca69
MaryNfs <mm10arya@gmail.com>
2026-03-31 23:27:19 +0800

    added tests and description for longestSubarray

--- Go/Leetcode/longest_subarray/longest_subarray_test.go
+++ Go/Leetcode/longest_subarray/longest_subarray_test.go
@@ -2,6 +2,20 @@ package longest_subarray
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestLongestSubarray(t *testing.T) {
+	cases := []struct{
+		nums []int
+		out int
+	}{
+		{[]int{1,1,0,1},3},
+		{[]int{0,1,1,1,0,1,1,0,1},5},
+		{[]int{1,1,1},2},
+		{[]int{0,1},1},
+	}
+	for _,c := range cases {
+		res := longestSubarray(c.nums)
+		if res != c.out {
+			t.Errorf("longestSubarray(%v)=%v, but expected %v",c.nums,res,c.out)
+		}
+	}
 }
