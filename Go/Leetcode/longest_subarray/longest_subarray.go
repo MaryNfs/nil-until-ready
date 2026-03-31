405332c5fa2a9cdac51e2d24af027a94730c61c3
MaryNfs <mm10arya@gmail.com>
2026-03-31 23:22:33 +0800

    added solution for longestSubarray

--- Go/Leetcode/longest_subarray/longest_subarray.go
+++ Go/Leetcode/longest_subarray/longest_subarray.go
@@ -1,6 +1,20 @@
 package longest_subarray
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func longestSubarray(nums []int) int {
+	count, res, zer := 0, 0, 0
+	l := -1
+	for r := 0; r < len(nums); r++ {
+		if nums[r] == 0 {
+			zer = 1
+			count = r - l - 1
+			l = r
+		} else {
+			count++
+		}
+		res = max(res, count)
+	}
+	if zer == 0 {
+		res = len(nums) - 1
+	}
+	return res
 }
