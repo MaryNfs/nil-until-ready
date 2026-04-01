e02dcc419c799e2ec7f6214219b6a172453f3455
MaryNfs <mm10arya@gmail.com>
2026-04-02 00:15:44 +0800

    added solution for reverseBits

--- Go/Leetcode/reverse_bits/reverse_bits.go
+++ Go/Leetcode/reverse_bits/reverse_bits.go
@@ -1,6 +1,16 @@
 package reverse_bits
 
-// TODO: implement solution
-func Solve() {
-	// implement
+// Not cool Question
+func reverseBits(n int) int {
+	res := 0
+	for _ = range 32 {
+        // left shift res by 1
+		res = res << 1
+        // add n%2 to res
+		bit := n % 2
+		res += bit
+        // right shift n by 1
+		n = n >> 1
+	}
+	return res
 }
