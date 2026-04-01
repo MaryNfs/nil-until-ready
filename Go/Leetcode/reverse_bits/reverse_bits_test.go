463ba6cfedbb51ceedf003759f491e08143e1540
MaryNfs <mm10arya@gmail.com>
2026-04-02 00:20:08 +0800

    added tests and description for reverseBits

--- Go/Leetcode/reverse_bits/reverse_bits_test.go
+++ Go/Leetcode/reverse_bits/reverse_bits_test.go
@@ -2,6 +2,18 @@ package reverse_bits
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestReverseBits(t *testing.T) {
+	cases := []struct {
+		num int
+		out int
+	}{
+		{43261596, 964176192},
+		{2147483644, 1073741822},
+	}
+	for _, c := range cases {
+		res := reverseBits(c.num)
+		if res != c.out {
+			t.Errorf("reverseBits(%v)=%v, but expected %v", c.num, res, c.out)
+		}
+	}
 }
