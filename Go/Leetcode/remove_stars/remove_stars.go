5ba01ba91b69010f37e75ed3f9220ca32c93cac2
MaryNfs <mm10arya@gmail.com>
2026-04-06 00:41:27 +0800

    added the accepted solution for removeStars

--- Go/Leetcode/remove_stars/remove_stars.go
+++ Go/Leetcode/remove_stars/remove_stars.go
@@ -6,13 +6,28 @@ import (
 )
 
 // Timelimit solution
+// func removeStars(s string) string {
+// 	arr := strings.Split(s, "")
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] == "*" {
+// 			arr = slices.Delete(arr, i-1, i+1)
+// 			i = i - 2
+// 		}
+// 	}
+// 	return strings.Join(arr, "")
+// }
+
+// accepted solution
+// define stack array with type byte
+// no need to convert slice to string and vice versa
 func removeStars(s string) string {
-	arr := strings.Split(s, "")
-	for i := 0; i < len(arr); i++ {
-		if arr[i] == "*" {
-			arr = slices.Delete(arr, i-1, i+1)
-			i = i - 2
-		}
+	stack := make([]byte,0)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '*' {
+			stack = stack[:len(stack)-1]
+		}else{
+            stack = append(stack,s[i])
+        }
 	}
-	return strings.Join(arr, "")
+	return string(stack)
 }
