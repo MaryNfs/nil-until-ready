bdb9c99c0e7d0e842d0137158de38035fd722e59
MaryNfs <mm10arya@gmail.com>
2026-04-05 23:16:58 +0800

    added solution for removeStars

--- Go/Leetcode/remove_stars/remove_stars.go
+++ Go/Leetcode/remove_stars/remove_stars.go
@@ -1,6 +1,18 @@
 package remove_stars
 
-// TODO: implement solution
-func Solve() {
-	// implement
+import (
+	"slices"
+	"strings"
+)
+
+// Timelimit solution
+func removeStars(s string) string {
+	arr := strings.Split(s, "")
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == "*" {
+			arr = slices.Delete(arr, i-1, i+1)
+			i = i - 2
+		}
+	}
+	return strings.Join(arr, "")
 }
