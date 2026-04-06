bcf4c58a8b6cfb4266e39a405f85ce4a951e3e56
MaryNfs <mm10arya@gmail.com>
2026-04-06 22:50:13 +0800

    added tests and description for asteroidCollision

--- Go/Leetcode/asteroid_collision/asteroid_collision_test.go
+++ Go/Leetcode/asteroid_collision/asteroid_collision_test.go
@@ -1,7 +1,24 @@
 package asteroid_collision
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestAsteroidCollision(t *testing.T) {
+	cases := []struct {
+		asts []int
+		out  []int
+	}{
+		{[]int{5, 10, -5}, []int{5, 10}},
+		{[]int{8, -8}, []int{}},
+		{[]int{10, 2, -5}, []int{10}},
+		{[]int{3, 5, -6, 2, -1, 4}, []int{-6, 2, 4}},
+	}
+	for _, c := range cases {
+		res := asteroidCollision(c.asts)
+		if !slices.Equal(res, c.out) {
+			t.Errorf("asteroidCollision(%v)=%v but expected %v", c.asts, res, c.out)
+		}
+	}
 }
