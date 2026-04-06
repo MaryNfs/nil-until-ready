c928628ea0cb11fb6e9322d8a4297678006ce22e
MaryNfs <mm10arya@gmail.com>
2026-04-06 22:44:17 +0800

    added solution for asteroidCollision

--- Go/Leetcode/asteroid_collision/asteroid_collision.go
+++ Go/Leetcode/asteroid_collision/asteroid_collision.go
@@ -1,6 +1,22 @@
 package asteroid_collision
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func asteroidCollision(asteroids []int) []int {
+	var stack []int
+	for i := 0; i < len(asteroids); i++ {
+		for len(stack) > 0 && asteroids[i] < 0 && stack[len(stack)-1] > 0 {
+			diff := asteroids[i] + stack[len(stack)-1]
+			if diff < 0 {
+				stack = stack[:len(stack)-1]
+			} else if diff > 0 {
+				asteroids[i] = 0
+			} else {
+				asteroids[i] = 0
+				stack = stack[:len(stack)-1]
+			}
+		}
+		if asteroids[i] != 0 {
+			stack = append(stack, asteroids[i])
+		}
+	}
+	return stack
 }
