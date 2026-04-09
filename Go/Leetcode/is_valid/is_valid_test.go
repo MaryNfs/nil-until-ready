ee5cae4697ec5ca4b15ed0f4cca71947ddfab36f
MaryNfs <mm10arya@gmail.com>
2026-04-10 00:03:38 +0800

    added tests for Valid Parentheses

--- Go/Leetcode/is_valid/is_valid_test.go
+++ Go/Leetcode/is_valid/is_valid_test.go
@@ -2,6 +2,21 @@ package is_valid
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestIsValid(t *testing.T) {
+	cases := []struct{
+		s string
+		out bool
+	}{
+		{"()",true},
+		{"()[]{}",true},
+		{"(]",false},
+		{"([)]",false},
+		{"]",false},
+	}
+	for _,c := range cases{
+		res := isValid(c.s)
+		if res != c.out{
+			t.Errorf("isValid(%v)=%v, but expected %v",c.s,res,c.out)
+		}
+	}
 }
