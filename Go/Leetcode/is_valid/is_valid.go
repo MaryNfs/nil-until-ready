90f6fe95181a6048fa9e204b398718116c280688
MaryNfs <mm10arya@gmail.com>
2026-04-09 10:00:40 +0800

    added solution for Valid Parentheses

--- Go/Leetcode/is_valid/is_valid.go
+++ Go/Leetcode/is_valid/is_valid.go
@@ -1,6 +1,28 @@
 package is_valid
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func isValid(s string) bool {
+	var stack []byte
+
+	open := map[byte]byte{'(': ')', '[': ']', '{': '}'}
+	close := map[byte]byte{')': '(', ']': '[', '}': '{'}
+
+	for i := 0; i < len(s); i++ {
+		_, ok1 := open[s[i]]
+		if ok1 {
+			stack = append(stack, s[i])
+		}
+		c, ok2 := close[s[i]]
+		if ok2 {
+			if len(stack) > 0 && c == stack[len(stack)-1] {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		}
+	}
+	if len(stack) == 0 {
+		return true
+	} else {
+		return false
+	}
 }
