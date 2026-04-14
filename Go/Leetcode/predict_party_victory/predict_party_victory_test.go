66200b44e6d21f95e26e87bb544a8cc63ef46b9b
MaryNfs <mm10arya@gmail.com>
2026-04-14 23:49:01 +0800

    added test for predictPartyVictory

--- Go/Leetcode/predict_party_victory/predict_party_victory_test.go
+++ Go/Leetcode/predict_party_victory/predict_party_victory_test.go
@@ -2,6 +2,19 @@ package predict_party_victory
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestPredictPartyVictory(t *testing.T) {
+	cases := []struct {
+		input  string
+		output string
+	}{
+		{"RD", "Radiant"},
+		{"RDD", "Dire"},
+		{"DDRRR", "Dire"},
+	}
+	for _, c := range cases {
+		res := predictPartyVictory(c.input)
+		if res != c.output{
+			t.Errorf("predictPartyVictory(%v)=%v, but expected %v",c.input,res,c.output)
+		}
+	}
 }
