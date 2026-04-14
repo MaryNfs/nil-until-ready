68307bbe7624c94e2fd93ceb0700757441b113a3
MaryNfs <mm10arya@gmail.com>
2026-04-14 23:45:52 +0800

    added predictPartyVictory solution

--- Go/Leetcode/predict_party_victory/predict_party_victory.go
+++ Go/Leetcode/predict_party_victory/predict_party_victory.go
@@ -1,6 +1,30 @@
 package predict_party_victory
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func predictPartyVictory(senate string) string {
+	dq := make([]int, 0)
+	rq := make([]int, 0)
+	for i, c := range senate {
+		if c == 'R' {
+			rq = append(rq, i)
+		} else {
+			dq = append(dq, i)
+		}
+	}
+	for len(dq) > 0 && len(rq) > 0 {
+		dTurn := dq[0]
+		dq = dq[1:]
+		rTurn := rq[0]
+		rq = rq[1:]
+		if dTurn > rTurn {
+			rq = append(rq, rTurn+len(senate))
+		} else {
+			dq = append(dq, dTurn+len(senate))
+		}
+	}
+
+	if len(dq) > 0 {
+		return "Dire"
+	} else {
+		return "Radiant"
+	}
 }
