25aab270fcbe32bb178169938856aa5ed5ab6b08
MaryNfs <mm10arya@gmail.com>
2026-04-14 09:43:12 +0800

    added test cases for decodeString

--- Go/Leetcode/decode_string/decode_string_test.go
+++ Go/Leetcode/decode_string/decode_string_test.go
@@ -2,6 +2,21 @@ package decode_string
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestDecodeString(t *testing.T) {
+	cases := []struct {
+		input  string
+		output string
+	}{
+		{"3[a]2[bc]", "aaabcbc"},
+		{"3[a2[c]]", "accaccacc"},
+		{"2[abc]3[cd]ef", "abcabccdcdcdef"},
+		{"111[bc]", "bcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbcbc"},
+	}
+
+	for _, c := range cases {
+		res := decodeString(c.input)
+		if res != c.output {
+			t.Errorf("decodeString(%v)=%v but expected %v", c.input, res, c.output)
+		}
+	}
 }
