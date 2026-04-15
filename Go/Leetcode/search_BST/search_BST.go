2825a5be624ab9d6ca818948c8baeae203008f4a
MaryNfs <mm10arya@gmail.com>
2026-04-15 23:49:00 +0800

    added solution for searchBST

--- Go/Leetcode/search_BST/search_BST.go
+++ Go/Leetcode/search_BST/search_BST.go
@@ -1,6 +1,21 @@
 package search_BST
 
-// TODO: implement solution
-func Solve() {
-	// implement
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root != nil {
+		if root.Val == val {
+			return root
+		} else if root.Val > val {
+			return searchBST(root.Left, val)
+		} else if root.Val < val {
+			return searchBST(root.Right, val)
+		}
+	}
+	return nil
 }
