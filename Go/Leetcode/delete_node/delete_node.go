4115b801a2402452ca7567ac55b0f3e3e3a01a55
MaryNfs <mm10arya@gmail.com>
2026-04-17 01:11:11 +0800

    added solution for deleteNode

--- Go/Leetcode/delete_node/delete_node.go
+++ Go/Leetcode/delete_node/delete_node.go
@@ -1,6 +1,33 @@
 package delete_node
 
-// TODO: implement solution
-func Solve() {
-	// implement
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root != nil {
+		if root.Val == key {
+			if root.Right == nil {
+				return root.Left
+			} else if root.Left == nil {
+				return root.Right
+			}
+			// find the min
+			cur := root.Right
+			for cur.Left != nil {
+				cur = cur.Left
+			}
+			root.Val = cur.Val
+			root.Right = deleteNode(root.Right, root.Val)
+		} else if root.Val > key {
+			root.Left = deleteNode(root.Left, key)
+		} else if root.Val < key {
+			root.Right = deleteNode(root.Right, key)
+		}
+		return root
+	}
+	return nil
 }
