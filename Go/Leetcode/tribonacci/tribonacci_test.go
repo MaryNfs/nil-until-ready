3c94d7860bcd90c8f9d90fca55c15f20968dabd6
MaryNfs <mm10arya@gmail.com>
2026-04-18 17:59:07 +0800

    added testst for tribonacci

--- Go/Leetcode/tribonacci/tribonacci_test.go
+++ Go/Leetcode/tribonacci/tribonacci_test.go
@@ -2,6 +2,19 @@ package tribonacci
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestTribonacci(t *testing.T) {
+	cases := []struct {
+		n   int
+		out int
+	}{
+		{4, 4},
+		{25, 1389537},
+		{0, 0},
+	}
+	for _, c := range cases {
+		res := tribonacci(c.n)
+		if res != c.out {
+			t.Errorf("tribonacci(%v)=%v but expected %v", c.n, res, c.out)
+		}
+	}
 }
