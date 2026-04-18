7e0a294158836ee3606a3ecc1d8b1873485fe510
MaryNfs <mm10arya@gmail.com>
2026-04-18 17:55:31 +0800

    added solution for tribonacci

--- Go/Leetcode/tribonacci/tribonacci.go
+++ Go/Leetcode/tribonacci/tribonacci.go
@@ -1,6 +1,20 @@
 package tribonacci
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func tribonacci(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 || n == 2 {
+		return 1
+	} else {
+		sum := make([]int, n+1)
+		sum[0] = 0
+		sum[1] = 1
+		sum[2] = 1
+
+		for i := 3; i <= n; i++ {
+			sum[i] = sum[i-1] + sum[i-2] + sum[i-3]
+		}
+		return sum[n]
+	}
+
 }
