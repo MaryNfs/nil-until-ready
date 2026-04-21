5a3b30349273e202eb64936a76b761def32dfb41
MaryNfs <mm10arya@gmail.com>
2026-04-21 09:09:56 +0800

    lame solution for successful_pairs

--- Go/Leetcode/successful_pairs/successful_pairs.go
+++ Go/Leetcode/successful_pairs/successful_pairs.go
@@ -1,6 +1,16 @@
 package successful_pairs
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func successfulPairs(spells []int, potions []int, success int64) []int {
+    res := make([]int,0)
+    for _,s := range spells {
+        r:=0
+        for _,p := range potions {
+            num := s*p
+            if int64(num) >= success {
+                r ++
+            }
+        }
+        res =append(res, r)
+    }
+    return res
 }
