b7e3d5f89cb215b8c67cc58ef9d54bd1507a3342
MaryNfs <mm10arya@gmail.com>
2026-04-21 09:43:29 +0800

    added binary search solution for successful_pairs

--- Go/Leetcode/successful_pairs/successful_pairs.go
+++ Go/Leetcode/successful_pairs/successful_pairs.go
@@ -1,16 +1,43 @@
 package successful_pairs
 
+import "slices"
+
+// O(n*m)
+// func successfulPairs(spells []int, potions []int, success int64) []int {
+// 	res := make([]int, 0)
+// 	for _, s := range spells {
+// 		r := 0
+// 		for _, p := range potions {
+// 			num := s * p
+// 			if int64(num) >= success {
+// 				r++
+// 			}
+// 		}
+// 		res = append(res, r)
+// 	}
+// 	return res
+// }
+
+// O(nlogm+mlogm) binary search
 func successfulPairs(spells []int, potions []int, success int64) []int {
-    res := make([]int,0)
-    for _,s := range spells {
-        r:=0
-        for _,p := range potions {
-            num := s*p
-            if int64(num) >= success {
-                r ++
-            }
-        }
-        res =append(res, r)
-    }
-    return res
+	res := make([]int, 0)
+	slices.Sort(potions)
+	for _, s := range spells {
+		index := len(potions)
+		l, r := 0, len(potions)-1
+
+		for l <= r {
+			m := (r + l) / 2
+			if int64(s)*int64(potions[m]) >= success {
+				r = m - 1
+				index = m
+			} else {
+				l = m + 1
+			}
+
+		}
+
+		res = append(res, len(potions)-index)
+	}
+	return res
 }
