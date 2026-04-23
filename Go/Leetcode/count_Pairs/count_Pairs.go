162b34c51ae0653099bd3b85050c87e23eb5a14e
MaryNfs <mm10arya@gmail.com>
2026-04-23 23:27:50 +0800

    added solution for countpairs

--- Go/Leetcode/count_Pairs/count_Pairs.go
+++ Go/Leetcode/count_Pairs/count_Pairs.go
@@ -1,6 +1,14 @@
 package count_Pairs
 
-// TODO: implement solution
-func Solve() {
-	// implement
+// o(n^2)
+func countPairs(nums []int, target int) int {
+    c:=0
+    for i:=0;i<len(nums);i++{
+        for j:=i+1;j<len(nums);j++{
+            if nums[i]+nums[j]< target{
+                c++
+            }
+        }
+    }
+    return c
 }
