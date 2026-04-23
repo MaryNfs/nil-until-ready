1d558b0765bef66c69e864790963f0f21966f34b
MaryNfs <mm10arya@gmail.com>
2026-04-23 23:33:24 +0800

    added tests and description for countPairs

--- Go/Leetcode/count_Pairs/count_Pairs_test.go
+++ Go/Leetcode/count_Pairs/count_Pairs_test.go
@@ -2,6 +2,19 @@ package count_Pairs
 
 import "testing"
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestCountPairs(t *testing.T) {
+	cases := []struct {
+		nums   []int
+		target int
+		out    int
+	}{
+		{[]int{-1, 1, 2, 3, 1}, 2, 3},
+		{[]int{-6, 2, 5, -2, -7, -1, 3}, -2, 10},
+	}
+	for _, c := range cases {
+		res := countPairs(c.nums, c.target)
+		if res != c.out {
+			t.Errorf("countPairs(%v,%v)=%v, but expected %v", c.nums, c.target, res, c.out)
+		}
+	}
 }
