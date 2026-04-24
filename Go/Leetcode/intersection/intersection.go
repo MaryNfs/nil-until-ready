f74e93cc9d15d2e3142e8fb25dc69488c79c3f53
MaryNfs <mm10arya@gmail.com>
2026-04-24 22:52:54 +0800

    added tests for intersection problem

--- Go/Leetcode/intersection/intersection.go
+++ Go/Leetcode/intersection/intersection.go
@@ -1,5 +1,6 @@
 package intersection
 
+// o(n*m) bad
 func intersection(nums1 []int, nums2 []int) []int {
 	res := make(map[int]int, 0)
 	for i := 0; i < len(nums1); i++ {
--- Go/Leetcode/intersection/intersection_test.go
+++ Go/Leetcode/intersection/intersection_test.go
@@ -1,7 +1,23 @@
 package intersection
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestIntersection(t *testing.T) {
+	cases := []struct {
+		nums1 []int
+		nums2 []int
+		out   []int
+	}{
+		{[]int{1, 2, 2, 1}, []int{2, 2}, []int{2}},
+		{[]int{4, 9, 5}, []int{9, 4, 9, 8, 4}, []int{4, 9}},
+	}
+	for _, c := range cases {
+		res := intersection(c.nums1, c.nums2)
+		if !slices.Equal(res, c.out) {
+			t.Errorf("intersection(%v,%v)=%v but expected %v", c.nums1, c.nums2, res, c.out)
+		}
+	}
 }
