4322ee7f38d9381e9532283a13795b3f0d51e430
MaryNfs <mm10arya@gmail.com>
2026-04-24 22:45:43 +0800

    added intersection solution

--- Go/Leetcode/intersection/intersection.go
+++ Go/Leetcode/intersection/intersection.go
@@ -1,6 +1,20 @@
 package intersection
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func intersection(nums1 []int, nums2 []int) []int {
+	res := make(map[int]int, 0)
+	for i := 0; i < len(nums1); i++ {
+		for j := 0; j < len(nums2); j++ {
+			if nums1[i] == nums2[j] {
+				_, ok := res[nums1[i]]
+				if !ok {
+					res[nums1[i]]++
+				}
+			}
+		}
+	}
+    final := make([]int,0)
+    for k,_ := range res{
+        final= append(final,k)
+    }
+	return final
 }
