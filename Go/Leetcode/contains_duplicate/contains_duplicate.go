033de8234149b9b8448ff0a69facddc813a2624d
MaryNfs <mm10arya@gmail.com>
2026-05-22 00:02:17 +0800

    added solution for containsDuplicate

--- Go/Leetcode/contains_duplicate/contains_duplicate.go
+++ Go/Leetcode/contains_duplicate/contains_duplicate.go
@@ -1,6 +1,14 @@
 package contains_duplicate
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func containsDuplicate(nums []int) bool {
+    hash := make(map[int]int,0)
+    for _,n:= range nums {
+        _,ok := hash[n]
+        if ok {
+            return true
+        }else{
+            hash[n]++
+        }
+    }
+    return false
 }
