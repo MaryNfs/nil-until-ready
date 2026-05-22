78987f4eccc3de78e195c92015c68a94c1bd3ab2
MaryNfs <mm10arya@gmail.com>
2026-05-22 23:15:10 +0800

    added solution & description for 350. Intersection of Two Arrays II

--- Go/Leetcode/intersect/intersect.go
+++ Go/Leetcode/intersect/intersect.go
@@ -1,6 +1,17 @@
 package intersect
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func intersect(nums1 []int, nums2 []int) []int {
+	hash := make(map[int]int, 0)
+	final := make([]int, 0)
+	for _, n := range nums1 {
+		hash[n]++
+	}
+	for _, n := range nums2 {
+		_, ok := hash[n]
+		if ok && hash[n] > 0 {
+			hash[n]--
+			final = append(final, n)
+		}
+	}
+	return final
 }
