1d5c674301789967b3e81d26be02974a39c3094d
MaryNfs <mm10arya@gmail.com>
2026-05-25 00:23:25 +0800

    added solution for createTargetArray

--- Go/Leetcode/create_target_array/create_target_array.go
+++ Go/Leetcode/create_target_array/create_target_array.go
@@ -1,6 +1,17 @@
 package create_target_array
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func createTargetArray(nums []int, index []int) []int {
+	length := len(nums)
+	t := make([]int, 0, length)
+	for i := 0; i < length; i++ {
+		t = append(t, 0)
+	}
+	for i := 0; i < length; i++ {
+		// 2. Shift elements to the right to make room
+		copy(t[index[i]+1:], t[index[i]:])
+
+		// 3. Insert the new value at the index
+		t[index[i]] = nums[i]
+	}
+	return t
 }
