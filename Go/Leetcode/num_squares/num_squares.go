59cfaefb65029305b7deca5da1e72341542883c5
MaryNfs <mm10arya@gmail.com>
2026-05-28 23:22:04 +0800

    added solution for numSquares

--- Go/Leetcode/num_squares/num_squares.go
+++ Go/Leetcode/num_squares/num_squares.go
@@ -1,6 +1,21 @@
 package num_squares
 
-// TODO: implement solution
-func Solve() {
-	// implement
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	for i:=0;i<=n;i++{
+		dp[i]=n
+	}
+	dp[0]=0
+
+	for i:=1;i<=n;i++{
+		for j:=1;j<=i;j++{
+			sq:= j*j
+			if i-sq <0 {
+				break
+			}
+			dp[i]=min(dp[i],dp[i-sq]+1)
+		}
+	}
+
+	return dp[n]
 }
