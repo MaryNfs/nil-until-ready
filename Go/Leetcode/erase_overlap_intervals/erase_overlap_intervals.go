f9374c6bf035033143c6ada6745476723f10e761
MaryNfs <mm10arya@gmail.com>
2026-06-10 00:18:08 +0800

    added erase_overlap_intervals solution

--- Go/Leetcode/erase_overlap_intervals/erase_overlap_intervals.go
+++ Go/Leetcode/erase_overlap_intervals/erase_overlap_intervals.go
@@ -1,6 +1,22 @@
 package erase_overlap_intervals
 
-// TODO: implement solution
-func Solve() {
-	// implement
+import "sort"
+
+func eraseOverlapIntervals(intervals [][]int) int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	res := 0
+	e := intervals[0][1]
+
+	for _, arr := range intervals[1:] {
+		if arr[0] >= e {
+			e = arr[1]
+		} else {
+			res++
+			e = min(e, arr[1])
+		}
+	}
+
+	return res
 }
