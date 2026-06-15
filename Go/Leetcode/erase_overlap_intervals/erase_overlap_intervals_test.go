f4e1262a779adba2afe069b6f402f398dd63c354
MaryNfs <mm10arya@gmail.com>
2026-06-15 23:44:43 +0800

    added test for eraseOverlapIntervals

--- Go/Leetcode/erase_overlap_intervals/erase_overlap_intervals_test.go
+++ Go/Leetcode/erase_overlap_intervals/erase_overlap_intervals_test.go
@@ -1,7 +1,24 @@
 package erase_overlap_intervals
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestSolve(t *testing.T) {
-	t.Fatal("write tests")
+func TestEraseOverlapIntervals(t *testing.T) {
+	cases := []struct {
+		intervals [][]int
+		out       int
+	}{
+		{[][]int{[]int{1, 2}, []int{2, 3}, []int{3, 4}, []int{1, 3}}, 1},
+		{[][]int{[]int{1, 2}, []int{1, 2}, []int{1, 2}}, 2},
+		{[][]int{[]int{1, 2}, []int{2, 3}}, 0},
+	}
+	for _, c := range cases {
+		res := eraseOverlapIntervals(c.intervals)
+
+		if res != c.out{
+			fmt.Printf("eraseOverlapIntervals(%v)=%v, but expected %v.",c.intervals,res,c.out)
+		}
+	}
 }
